c3348ed1265cd0ce24cc7fcaa765a2caabc00c92
dtynn <dtynn@163.com>
2013-11-15 18:14:42 +0800

    first

--- watcher.py
+++ watcher.py
@@ -1,7 +1,12 @@
 #coding=utf-8
 import pyinotify
-from pyinotify import WatchManager, Notifier, ThreadedNotifier,ProcessEvent, ExcludeFilter
+from pyinotify import WatchManager, Notifier, ProcessEvent, ExcludeFilter
 from optparse import OptionParser
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(levelname)1.1s %(asctime)1.19s %(module)s:%(lineno)d] %(message)s')
 
 
 class processHandler(ProcessEvent):
@@ -15,19 +20,27 @@ def optParser():
     #settings
     optp.add_option('-d', '--dir', help='directory',
                     dest='dir', default=None)
+    optp.add_option('-c', '--conf', help='config file',
+                    dest='conf', default=None)
 
     opts, args = optp.parse_args()
-    return opts.dir
+    return opts.dir, opts.conf
 
 
 def main():
-    targetDir = optParser()
+    basePath, confFile = optParser()
+    if not basePath:
+        logging.error('Need a path to watch')
+        return
+    if not confFile:
+        logging.error('Need a config file')
+        return
 
     wm = WatchManager()
     mask = pyinotify.IN_CLOSE_WRITE
     excl_list = ['^.*/m3u8$', ]
     excl = ExcludeFilter(excl_list)
-    wadd = wm.add_watch(targetDir, mask, rec=True, exclude_filter=excl)
+    wadd = wm.add_watch(basePath, mask, rec=True, exclude_filter=excl)
     notifier = Notifier(wm, processHandler())
     while True:
         try:
